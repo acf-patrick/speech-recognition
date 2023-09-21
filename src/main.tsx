ec23abe32fd3194d07129067953abf056abd1f78
acf-patrick <miharisoap@gmail.com>
2023-09-21 09:35:00 +0300

    disable text selection and context menu on prod mode

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
+if (import.meta.env.PROD) {
+  document.body.style.userSelect = "none";
+  document.body.oncontextmenu = (e) => {
+    e.preventDefault();
+  };
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
